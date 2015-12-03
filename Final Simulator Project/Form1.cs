06a8770e6ca61e327d29ebd34c1cfec689faeeb6
Youssef93 <youssef1993fayez@gmail.com>
2015-12-03 16:01:46 +0200

    tried making array on form1

--- Final Simulator Project/Form1.cs
+++ Final Simulator Project/Form1.cs
@@ -12,10 +12,12 @@ namespace Final_Simulator_Project
 {
     public partial class Form1 : Form
     {
-        public static bool gatecontainer_created = false;
-        GateContainer [] gatecontainer = new GateContainer[50];
+        public static bool[] gatecontainer_created = new bool[60];
+        
+        GateContainer[] gatecontainer = new GateContainer[50];
         int gatecontainer_counter = 0;
         public static Point point;
+        Rectangle[] SavedContainerRectangles = new Rectangle[50];
         public Form1()
         {
             InitializeComponent();
@@ -26,31 +28,52 @@ namespace Final_Simulator_Project
             if (GateContainer.ContainerRectangle.Contains(new Point(e.X, e.Y)))
             {
                 gatecontainer[gatecontainer_counter].Visible = true;
-                
+
             }
         }
 
         private void panel1_MouseMove(object sender, MouseEventArgs e)
         {
-            if (GateContainer.ContainerRectangle.Contains(new Point(e.X, e.Y)) && gatecontainer_created)
+            for (int i = 0; i <= gatecontainer_counter; i++)
             {
-                gatecontainer[gatecontainer_counter].Visible = true;
 
-            }
-            else if (gatecontainer_created)
-            {
-                GateContainer.Redraw_Gate_After_Visibility_Change = true;
-                gatecontainer[gatecontainer_counter].Visible = false;
+                if (SavedContainerRectangles[i].Contains(new Point(e.X, e.Y)) && gatecontainer_created[i])
+                {
+                    gatecontainer[i].Visible = true;
+
+                }
+                else if (gatecontainer_created[i])
+                {
+                    GateContainer.Redraw_Gate_After_Visibility_Change = true;
+                    gatecontainer[i].Visible = false;
+                }
             }
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            gatecontainer [gatecontainer_counter]= new GateContainer();
+            gatecontainer[gatecontainer_counter] = new GateContainer();
             panel1.Controls.Add(gatecontainer[gatecontainer_counter]);
             gatecontainer[gatecontainer_counter].Location = new Point(100, 100);
-            gatecontainer_created = true;
+            gatecontainer_created [gatecontainer_counter]= true;
+            SaveGate();
+            gatecontainer_counter++;
+        }
+        public void SaveGate()
+        {
+            SavedContainerRectangles[gatecontainer_counter] =  GateContainer.ContainerRectangle;
+        }
+        public void setAllBoolsFalse()
+        {
+            for (int i = 0; i <= 50; i++)
+            {
+                gatecontainer_created[i] = false;
+            }
+        }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            setAllBoolsFalse();
         }
     }
 }
